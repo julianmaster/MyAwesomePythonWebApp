5d5cecc7c91340ee213f80614b8fcabe28f72d44
Julien Maitre <julien.maitre@univ-lr.fr>
2024-04-30 15:28:55 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Input(BaseModel):
+    text: str
+
+
+# Define the app
+app = FastAPI(
+    title="MyAwesomePythonWebApp",
+    description="Hello API developer!",
+    version="0.1.0"
+)
+
+
+# Define the APIs
+@app.get("/")
+async def main():
+    return {"message": "Hello World"}
+
+
+# Define a POST operation
+@app.post("/submit")
+async def submit(input: Input):
+    return {"message": f"Data submitted is: {input.text}"}
